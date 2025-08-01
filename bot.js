a3b2d7004c0e7f47854c189a4664b3abed144ad8
Hugo <h.havet@icloud.com>
2025-08-01 10:00:00 +0200

    Random bot

---
+++ bot.js
@@ -0,0 +1,15 @@
+const moves = ["UP", "DOWN", "LEFT", "RIGHT", "STAY"];
+const actions = ["BOMB", "COLLECT", "NONE"];
+
+function getRandom(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function getBotAction() {
+  return {
+    move: getRandom(moves),
+    action: getRandom(actions),
+  };
+}
+
+module.exports = getBotAction;
--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const getBotAction = require('./bot');
+
 app.get('/action', (req, res) => {
-    res.json({ move: "RIGHT", action: "NONE" });
+  const action = getBotAction();
+  res.json(action);
 });
 
 app.listen(port, () => {
